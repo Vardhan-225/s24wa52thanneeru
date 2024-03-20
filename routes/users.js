4decdcc725a61e654a350b6ed8388299e7dcfc81
Akash Thanneeru <iamakash225@gmail.com>
2024-03-19 23:52:34 -0500

    Modified users endpoint to display product value

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// Create variables product and factor
+var product = 1;
+var factor = 1;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // Increment factor and multiply product
+  factor++;
+  product *= factor;
+
+  // Send response with the updated product value
+  res.send('Product is: ' + product);
 });
 
 module.exports = router;
