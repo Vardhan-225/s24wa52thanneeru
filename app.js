d91092bebbe6f8e623b2bc44f5c45daadb0d2bf9
Akash Thanneeru <iamakash225@gmail.com>
2024-03-19 23:20:21 -0500

    Pushing changes to routes and views to mydata to remote repo

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,11 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/mydata', mydataRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next) {[]
   next(createError(404));
 });
 
